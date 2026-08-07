33d3143a3f61822f4b527ecb543e9ce12d68a2d2
Mithil Salunkhe <58468909+mithils3@users.noreply.github.com>
2026-08-07 11:13:45 -0500

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <string>
+using namespace std;
+class Solution
+{
+public:
+    string encode(vector<string> &strs)
+    {
+        string output;
+        for (string& s : strs){
+            output += to_string(s.size()) + "_" + s;
+        }
+        return output;
+    }
+
+    vector<string> decode(string s)
+    {
+        vector<string> output;
+        string buffer;
+        
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '_') {
+                int length = stoi(buffer);
+                output.push_back(s.substr(i+1,length));
+                buffer.clear();
+                i += length;
+
+            } else {
+                buffer += s[i];
+            }
+
+        }
+        return output;
+    }
+};
