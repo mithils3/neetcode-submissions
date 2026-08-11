2c8a03dc4be6324abddb313684775d311642b223
Mithil Salunkhe <58468909+mithils3@users.noreply.github.com>
2026-08-11 13:11:30 -0500

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> dailyTemperatures(vector<int> &temperatures)
+    {
+        stack<pair<int, int>, vector<pair<int, int>>> s;
+        vector<int> output;
+        output.resize(temperatures.size());
+        for (unsigned int x = 0; x < temperatures.size(); x++)
+        {
+            while (!s.empty() && s.top().second < temperatures[x])
+            {
+                auto popped = s.top();
+                s.pop();
+                output[popped.first] = x - popped.first;
+            }
+            s.push({x, temperatures[x]});
+        }
+        return output;
+    }
+};
+// for this we want Next Greater Temperature to the right
